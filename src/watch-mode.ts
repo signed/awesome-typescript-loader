cc06d62bbf1d474585ad2708f5915a18e8e6d7c7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2020-06-05 18:28:58 +0200

    add steps on how to setup ts-strictify

--- src/watch-mode.ts
+++ src/watch-mode.ts
@@ -1,5 +1,5 @@
 export const WatchModeSymbol = Symbol('WatchMode')
-
+// the change
 export class CheckerPlugin {
 	apply(compiler) {
 		compiler.hooks.run.tapAsync('at-loader', function(params, callback) {
---
+++ steps.md
@@ -0,0 +1,7 @@
+1. enable strict and compile
+1. yarn add --dev ts-strictify
+1. yarn ts-strictify
+1. make change to `src/watch-mode.ts`
+1. add typecheck
+1. yarn add --dev concurrently
+1. concurrently
